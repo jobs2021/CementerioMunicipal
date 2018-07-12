72e1095e37a5c42a9121a19c1315bbae8f5b3076
Kevin Rivas <rl0185032015@unab.edu.sv>
2018-07-11 20:27:05 -0600

    correccion URLs

--- Views/default/footer.php
+++ Views/default/footer.php
@@ -1,6 +1,6 @@
 	
 </body>
-<script src="http://localhost/Views/static/js/jquery/jquery-3.3.1.min.js"></script>
-<script src="http://localhost/Views/static/js/bootstrap.min.js"></script>
-<script src="http://localhost/Views/static/js/bootstrap.bundler.min.js"></script>
+<script src="./Views/static/js/jquery/jquery-3.3.1.min.js"></script>
+<script src="./Views/static/js/bootstrap.min.js"></script>
+<script src="./Views/static/js/bootstrap.bundler.min.js"></script>
 </html>
