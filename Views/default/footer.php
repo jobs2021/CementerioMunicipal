73b703dc26b52a15d248fba5ace0576d92cde2de
jobs2021 <39226656+jobs2021@users.noreply.github.com>
2018-07-17 13:35:58 -0600

    popper.min.js integrado en vez de bundle.js

--- Views/default/footer.php
+++ Views/default/footer.php
@@ -3,4 +3,4 @@
 <script src="./Views/static/js/jquery/jquery-3.3.1.min.js"></script>
 <script src="./Views/static/js/popper.min.js"></script>
 <script src="./Views/static/js/bootstrap.min.js"></script>
-</html>
+</html> 
