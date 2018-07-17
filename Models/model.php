a68e571f3a87d26a32300bc4c5b3e9e8476e6c3e
jobs2021 <eserafintov@gmail.com>
2018-07-17 13:27:59 -0600

    vistas integradas

--- Models/model.php
+++ Models/model.php
@@ -3,7 +3,6 @@
 
 class enlacesPaginas{
 
-<<<<<<< HEAD
     public $links = array('inicio',array('cementerios','admincementerio','parcelas','verparcela'),array('titulos','creartitulo','repotrastitulo', 'vertitulo', 'eyetitulo'),'desconectar');
 
     public function enlacesPaginasModel($enlacesModel){
@@ -23,29 +22,6 @@ class enlacesPaginas{
     
         } else {
             $module = "Views/templates/404.php";
-=======
-    public $links = array('inicio','registrar','titulos','creartitulo','repotrastitulo', 'vertitulo', 'eyetitulo');
-
-    public function enlacesPaginasModel($enlacesModel){
-
-        if ($this->links[2]==$enlacesModel) {
-            $module = "Views/templates/titulos/".$enlacesModel.".php";
-    
-        }elseif ($this->links[3]==$enlacesModel) {
-            $module = "Views/templates/titulos/".$enlacesModel.".php";
-    
-        }elseif ($this->links[4]==$enlacesModel) {
-            $module = "Views/templates/titulos/".$enlacesModel.".php";
-
-        }elseif ($this->links[5]==$enlacesModel) {
-            $module = "Views/templates/titulos/".$enlacesModel.".php";
-
-        }elseif ($this->links[6]==$enlacesModel) {
-            $module = "Views/templates/titulos/".$enlacesModel.".php";
-
-        }else {
-            $module = "Views/templates/inicio.php";
->>>>>>> ad54a052f95e5277a481d4e3dd51b4f37b0e5567
         }
 
         return $module;
