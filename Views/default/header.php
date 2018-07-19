86604282fc32432ff564f241ac796f910cc98d6b
jobs2021 <eserafintov@gmail.com>
2018-07-19 12:18:01 -0600

    favicon agreado

--- Views/default/header.php
+++ Views/default/header.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $server;?>/Views/static/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo $server;?>/Views/static/assets/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $server;?>/Views/static/assets/favicon/favicon-16x16.png">
     <title>
         <?php echo $titulo; ?>
     </title>
